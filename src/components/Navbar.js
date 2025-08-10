a00210d80d06c38b77209db55e7b64786e08d430
Anirban Ghosh <officialanirbanghoshag@gmail.com>
2025-08-10 18:54:49 +0530

    improved bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // <-- Import Link
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
@@ -7,16 +7,29 @@ function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-black px-4 py-2">
-      <div className="container-fluid d-flex justify-content-between align-items-center w-100">
+      <div className="container-fluid">
 
-        {/* Left: Logo */}
+        {/* Logo */}
         <Link className="navbar-brand d-flex align-items-center" to="/">
           <img src="/logo.png" alt="Logo" width="180" className="me-2" />
         </Link>
 
-        {/* Center: Navigation */}
-        <div className="d-none d-lg-flex flex-grow-1 justify-content-center">
-          <ul className="navbar-nav text-white flex-row">
+        {/* Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Collapsible content */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mx-auto">
             <li className="nav-item px-3">
               <Link className="nav-link" to="/">Home</Link>
             </li>
@@ -27,24 +40,18 @@ function Navbar() {
               <Link className="nav-link" to="/services">Our Services</Link>
             </li>
 
-            {/* Courses Dropdown */}
+            {/* Courses dropdown */}
             <li
               className="nav-item dropdown px-3"
               onMouseEnter={() => setShowDropdown(true)}
               onMouseLeave={() => setShowDropdown(false)}
             >
-              <span className="nav-link dropdown-toggle">Courses</span>
+              <span className="nav-link dropdown-toggle" role="button">Courses</span>
               {showDropdown && (
                 <ul className="dropdown-menu show bg-light text-dark">
-                  <li>
-                    <Link className="dropdown-item" to="/courses/beginner">Beginner</Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/courses/intermediate">Intermediate</Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" to="/courses/advanced">Advanced</Link>
-                  </li>
+                  <li><Link className="dropdown-item" to="/courses/beginner">Beginner</Link></li>
+                  <li><Link className="dropdown-item" to="/courses/intermediate">Intermediate</Link></li>
+                  <li><Link className="dropdown-item" to="/courses/advanced">Advanced</Link></li>
                 </ul>
               )}
             </li>
@@ -56,12 +63,12 @@ function Navbar() {
               <Link className="nav-link" to="/gallery">Gallery</Link>
             </li>
           </ul>
-        </div>
 
-        {/* Right: Buttons */}
-        <div className="d-flex align-items-center gap-2">
-          <Link className="btn btn-outline-light" to="/classroom">Our Classroom</Link>
-          <Link className="btn btn-success text-white" to="/contact">Contact Us</Link>
+          {/* Right side buttons */}
+          <div className="d-flex gap-2 mt-2 mt-lg-0">
+            <Link className="btn btn-outline-light" to="/classroom">Our Classroom</Link>
+            <Link className="btn btn-success text-white" to="/contact">Contact Us</Link>
+          </div>
         </div>
       </div>
     </nav>
